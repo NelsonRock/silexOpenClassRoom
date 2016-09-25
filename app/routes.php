ef8b1dfb2cd684a0665e142c81a8e33af3ee0dab
Nelson Rodriguez <nelsao101@gmail.com>
2016-09-25 14:17:41 +0200

    segundo commit

---
+++ app/routes.php
@@ -0,0 +1,9 @@
+<?php
+ $app->get('/', function () {
+     require '../src/model.php';
+     $articles = getArticles();
+     ob_start();
+     require '../views/view.php';
+     $view = ob_get_clean();
+     return $view;
+ });
