157d8a75c4ed98e81aed9a03f96eb568765e6f80
Nelson Rodriguez <nelsao101@gmail.com>
2016-09-26 14:52:35 +0200

    Registramos twig como teplate

--- app/app.php
+++ app/app.php
@@ -9,7 +9,10 @@ ExceptionHandler::register();
 
 // Register service providers.
 $app->register(new Silex\Provider\DoctrineServiceProvider());
-
+// TwigServiceProvider para utilizar twig templates
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/../views';
+));
 // Register services.
 $app['dao.article'] = $app->share(function ($app) {
     return new silex\DAO\ArticleDAO($app['db']);
