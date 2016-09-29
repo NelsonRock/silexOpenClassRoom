836648d0b9f9614f761fe2f939ec0eaadba2ab05
Nelson Rodriguez <nelsao101@gmail.com>
2016-09-29 12:32:36 +0200

    controlador modificado con match para acceder a la ruta mediante http, get, post, ->get solo accede a GET, recuperamos el id, verificamos el usuario, recuperamos con ['user'],

--- app/routes.php
+++ app/routes.php
@@ -1,16 +1,39 @@
 <?php
 
 use Symfony\Component\HttpFoundation\Request;
+use silex\Domain\Comment;
+use silex\Form\Type\CommentType;
+
 //Home page
  $app->get('/', function () use ($app) {
      $articles = $app['dao.article']->findAll();
      return $app['twig']->render('index.html.twig', array('articles' => $articles));
  })->bind('home');
 
-$app->get('/article/{id}', function ($id) use ($app) {
+//Articles with comments details
+$app->match('/article/{id}', function ($id, Request $request) use ($app) {
     $article = $app['dao.article']->find($id);
+    $commentFormView = null;
+    if ($app['security.authorization_checker']->isGranted('IS_AUTHENTICADED_FULLY')) {
+        //User can poste Comment
+        $comment = new Comment();
+        $comment->setArticle($article);
+        $user = $app['user'];
+        $comment->setAuthor($user);
+        $commentForm = $app['form.factory']->create(new CommentType, $comment);
+        $commentForm->handleRequest($request);
+        if ($commentForm->isSubmitted() && $commentForm->isValid() ) {
+            $app['dao.comment']->save($comment);
+            $app['session']->getFlashBag()->add('succes', 'Tu comentario fue registrado con exito');
+        }
+        $commentFormView = $commentForm->createView();
+    }
     $comments = $app['dao.comment']->findAllByArticle($id);
-    return $app['twig']->render('article.html.twig' , array('article'=> $article, 'comments' => $comments ));
+    return $app['twig']->render('article.html.twig' , array(
+         'article'=> $article, 'comments' => $comments,
+         'comments' => $comments,
+         'commentForm' => $commentFormView
+         ));
 })->bind('article');
 
 $app->get('/login', function (Request $request) use ($app) {
